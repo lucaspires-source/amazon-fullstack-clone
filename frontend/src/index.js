ab7f8d57a1befc673ae4537caeb48bb24e6ebfca
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-29 10:12:35 -0300

    added redux to the project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Provider from 'react-redux'
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
 import './index.css';
+import store from './store'
+
 ReactDOM.render(
-  
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
