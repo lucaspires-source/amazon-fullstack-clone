c73c27d6e72d35c26ea0a996653a487a8854eea4
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-12-30 15:05:31 -0300

    created the backend for signin process

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,19 @@ import express from "express";
 import mongoose from 'mongoose'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
+import dotenv from 'dotenv'
+
+
+dotenv.config();
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 mongoose.connect( process.env.MONGODB_URL || 'mongodb://localhost/amazona',{
   useNewUrlParser:true,
   useUnifiedTopology:true,
   useCreateIndex:true
 })
-const port = process.env.PORT || 5000;
+
 
 app.use('/api/users', userRouter)
 app.use('/api/products',productRouter)
@@ -16,10 +22,11 @@ app.get("/", (req, res) => {
   res.send("Server is Ready");
 });
 
+app.use((err,req,res,next)=>{
+  res.status(500).send({message:err.message});
+});
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
 });
 
-app.use((err,req,res,next)=>{
-  res.status(500).send({message:err.message});
-});
